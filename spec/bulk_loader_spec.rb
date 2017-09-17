72015e3a1dc9ca264316713d37815ac0f8497252
Keiji Yoshimi <walf443@gmail.com>
2017-09-17 12:02:19 +0900

    remove generated test.

--- spec/bulk_loader_spec.rb
+++ spec/bulk_loader_spec.rb
@@ -6,8 +6,4 @@ RSpec.describe BulkLoader do
   it 'has a version number' do
     expect(BulkLoader::VERSION).not_to be nil
   end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
-  end
 end
