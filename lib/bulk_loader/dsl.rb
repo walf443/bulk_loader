10f0b43f2615eb4294b09ce7344c43f5071092a8
Keiji Yoshimi <walf443@gmail.com>
2020-09-26 11:14:35 +0900

    added doc for autoload option

--- lib/bulk_loader/dsl.rb
+++ lib/bulk_loader/dsl.rb
@@ -6,6 +6,7 @@ module BulkLoader
       # getter for +BulkLoader::ClassAttribute+
       # If you pass name, mapping, options argument, you can define loader
       # if you does not want to export name to object, pass export: false to options.
+      # if you would like to raise error when implicitly calling loader, pass autoload: false to options.
       def bulk_loader(*args, export: nil, autoload: true, default: nil, &block)
         @bulk_loader ||= if superclass.respond_to?(:bulk_loader)
                            superclass.bulk_loader.dup
